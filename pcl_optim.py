6635b53277f5e5ce0df1393e6cc3d7e546195a0c
Martin Halaj <mhalaj999@gmail.com>
2026-03-12 22:45:21 +0100

    Perf: Stage 1 improvements

--- pcl_optim.py
+++ pcl_optim.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 import cv2
 import numpy as np
@@ -13,18 +14,32 @@ import scipy
 import torch
 import rerun as rr
 import trimesh
+from timm.models import model_parameters
 from tqdm import tqdm
 import time
+from mhr.mhr import MHR
+from pathlib import Path
 
-from utils.image import load_image, is_image
-from utils.measure import get_measurements
-from utils.pointcloud import get_moge_pointcloud, get_scaled_pointcloud
+# from utils.image import load_image, is_image
+# from utils.measure import get_measurements
+# from utils.pointcloud import get_moge_pointcloud, get_scaled_pointcloud
 
 class SinglePoseMHR(torch.nn.Module):
     def __init__(self, fname, subsample=5000, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.mhr_model = torch.jit.load("assets/mhr_model.pt")
-        self.register_buffer('faces', self.mhr_model.character_torch.mesh.faces.unsqueeze(0))
+        # self.mhr_model = torch.jit.load("assets/mhr_model.pt")
+        # self.register_buffer('faces', self.mhr_model.character_torch.mesh.faces.unsqueeze(0))
+        self.mhr_model = MHR.from_files(
+            folder=Path("assets/assets"),
+            device=torch.device("cuda"),
+            lod=4
+        )
+        faces = torch.as_tensor(
+            self.mhr_model.character.mesh.faces,
+            dtype=torch.float64,
+            device="cuda"
+        )
+        self.register_buffer("faces", faces.unsqueeze(0))
 
         rr.init(f"Viewer - Single Pose", spawn=True)
         rr.log("world", rr.ViewCoordinates.RIGHT_HAND_Z_UP, static=True)
@@ -37,15 +52,23 @@ class SinglePoseMHR(torch.nn.Module):
         pcl_mesh.vertices[:, 1] *= -1
         pcl_mesh.vertices[:, 2] *= -1
 
-        self.pcl = Pointclouds(torch.Tensor(pcl_mesh.vertices).unsqueeze(0) / 1000).cuda()
+        # self.pcl = Pointclouds(torch.Tensor(pcl_mesh.vertices).unsqueeze(0) / 1000).cuda()
+        self.pcl = Pointclouds(
+            torch.tensor(pcl_mesh.vertices, dtype=torch.float32, device="cuda").unsqueeze(0) / 1000
+        )
+
         self.colors = pcl_mesh.colors
 
         print("Data loaded")
 
         self.identity = torch.nn.Parameter(torch.zeros([1, 45]), requires_grad=True)
-        self.expr = torch.nn.Parameter(torch.zeros([1, 72]), requires_grad=True)
+        # self.expr = torch.nn.Parameter(torch.zeros([1, 72]), requires_grad=True)
+        # Excluding face expression from optimizing
+        self.expr = self.register_buffer("expr", torch.zeros([1, 72], device="cuda"))
         self.pose = torch.nn.Parameter(torch.zeros([1, 204]), requires_grad=True)
         self.t = torch.nn.Parameter(torch.zeros([1, 3]), requires_grad=True)
+
+        self.register_buffer("finger_pose_mask", self._create_finger_mask())
 
     def _start_timer(self):
         e = torch.cuda.Event(enable_timing=True)
@@ -58,38 +81,64 @@ class SinglePoseMHR(torch.nn.Module):
         torch.cuda.synchronize()
         return start_event.elapsed_time(end)  # milliseconds
 
-    def forward(self):
-        t1 = self._start_timer()
-        mean_model_vertices, skel_state = (
-            self.mhr_model(
-                model_parameters=self.pose,
-                identity_coeffs=self.identity,
-                face_expr_coeffs=self.expr,
-            )
-        )
-        mhr_time = self._end_timer(t1)
+    def _create_finger_mask(self):
+        # https://github.com/facebookresearch/MHR/blob/main/tools/mhr_smpl_conversion/conversion.py#L658
+        num_total_params = self.pose.shape[1]
 
-        t2 = self._start_timer()
+        num_pose_param = int(self.mhr_model.character.parameter_transform.pose_parameters.sum() - 6)
+        num_scale_params = int(self.mhr_model.character.parameter_transform.scaling_parameters.sum())
+
+        lbs_parameter_names = self.mhr_model.character.parameter_transform.names[
+            6: 6 + num_pose_param + num_scale_params
+        ]
+
+        finger_parts = {"index", "middle", "ring", "pinky", "thumb"}
+
+        mask = torch.ones(num_total_params, dtype=torch.float32, device="cuda")
+
+        for i, name in enumerate(lbs_parameter_names[:num_pose_param]):
+            if any(part in name.lower() for part in finger_parts):
+                mask[6 + i] = 0.0
+
+        return mask.unsqueeze(0)  # shape [1, 204]
+
+    def forward(self):
+        # t1 = self._start_timer()
+        # mean_model_vertices, skel_state = (
+        #     self.mhr_model(
+        #         model_parameters=self.pose,
+        #         identity_coeffs=self.identity,
+        #         face_expr_coeffs=self.expr,
+        #     )
+        # )
+        mean_model_vertices, skel_state = self.mhr_model(
+            identity_coeffs=self.identity,
+            model_parameters=self.pose,
+            face_expr_coeffs=self.expr
+        )
+        # mhr_time = self._end_timer(t1)
+
+        # t2 = self._start_timer()
         mean_model_vertices /= 100
         mean_model_vertices[:, :, 1] *= -1
         mean_model_vertices[:, :, 2] *= -1
         mean_model_vertices += self.t.reshape(-1, 1, 3)
-        transform_time = self._end_timer(t2)
+        # transform_time = self._end_timer(t2)
 
-        t3 = self._start_timer()
+        # t3 = self._start_timer()
         meshes = Meshes(mean_model_vertices, self.faces)
-        mesh_creation_time = self._end_timer(t3)
+        # mesh_creation_time = self._end_timer(t3)
 
         # print("Computin loss")
-        t4 = self._start_timer()
+        # t4 = self._start_timer()
         # pcl_loss = pytorch3d.loss.point_mesh_face_distance(meshes, self.pcl)
         # ======================================================================
-        sampled_points = sample_points_from_meshes(meshes, num_samples=5000)
+        sampled_points = sample_points_from_meshes(meshes, num_samples=40_000)
         pcl_loss, _ = pytorch3d.loss.chamfer_distance(sampled_points, self.pcl.points_padded())
-        loss_time = self._end_timer(t4)
+        # loss_time = self._end_timer(t4)
 
-        total_time = mhr_time + transform_time + mesh_creation_time + loss_time
-        print(f"[ms] MHR={mhr_time:.2f} T={transform_time:.2f} M={mesh_creation_time:.2f} L={loss_time:.2f} Total={total_time:.2f}")
+        # total_time = mhr_time + transform_time + mesh_creation_time + loss_time
+        # print(f"[ms] MHR={mhr_time:.2f} T={transform_time:.2f} M={mesh_creation_time:.2f} L={loss_time:.2f} Total={total_time:.2f}")
 
         return mean_model_vertices, pcl_loss
 
@@ -112,20 +161,34 @@ if __name__ == '__main__':
     image_dir = 'D:/Research/data/antropo/x1'
     data_dir = 'D:/Research/data/antropo/x1'
 
-    fname = 'D:/Research/data/antropo/pointcloud/body_processed.ply'
+    # fname = 'D:/Research/data/antropo/pointcloud/body_processed.ply'
+    fname = "pointclouds/body_processed.ply"
     # fnames = ['IMG_9581.jpeg']
 
     model = SinglePoseMHR(fname)
     model.cuda()
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-    pbar = tqdm(range(500))
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.02)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer,
+        mode='min',
+        factor=0.5,
+        patience=10,
+        min_lr=1e-5
+    )
+    pbar = tqdm(range(600))
     for i in pbar:
         optimizer.zero_grad()
         vertices, loss = model()
         loss.backward()
+        with torch.no_grad():
+            model.pose.grad *= model.finger_pose_mask
+
         optimizer.step()
-        pbar.set_description(f"Loss: {loss.item():0.6f}")
+
+        scheduler.step(loss.item())
+        currentLr = optimizer.param_groups[0]['lr']
+
+        pbar.set_description(f"Loss: {loss.item():0.6f}, LR: {currentLr:0.6f}")
         if i % 10 == 0:
             model.visualize(i, vertices)
